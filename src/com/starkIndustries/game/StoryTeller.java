0a7bcdf3e3964b4a50e5f7e5ad0e974a94fd54b0
Ricky Zhang <hongxiaozh@gmail.com>
2020-08-27 14:35:10 -0700

    getStory method added

--- src/com/starkIndustries/game/StoryTeller.java
+++ src/com/starkIndustries/game/StoryTeller.java
@@ -1,5 +1,25 @@
 package com.starkIndustries.game;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class StoryTeller {
-    //TODO：write a method to load JSON objects
+    //TODO：write a method to load JSON objects'
+    public JsonObject getStory(File path)  {
+        JsonElement jsonFile = null;
+        try {
+            jsonFile = JsonParser.parseReader(new FileReader(path));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return jsonFile.getAsJsonObject();
+
+    }
+
+    //TODO: write a method to tell Story
 }
--- src/com/startIndustries/client/PlayerClient.java
+++ src/com/startIndustries/client/PlayerClient.java
@@ -1,7 +1,26 @@
 package com.startIndustries.client;
 
+import com.google.gson.JsonObject;
+import com.starkIndustries.game.StoryTeller;
+
+import java.io.File;
+
 public class PlayerClient {
     public static void main(String[] args) {
+        File path = new File("resources /story.json");
+
+        StoryTeller storyTeller = new StoryTeller();
+
+        JsonObject newStory = storyTeller.getStory(path);
+
+
+        System.out.println("You wakes up suddenly with a tube up your nose,laying on a cot in a cave. you rip out " +
+                "the" +
+                " tube and tries to pick up a cup on the table beside you, but fail. you notice a man shaving and " +
+                "tries to reach further but is stopped by wires.\n");
+
+        System.out.println(newStory.get("Yinsen: I wouldn't do that if I were you").getAsJsonObject().get("description"));;
+
 
     }
 }
