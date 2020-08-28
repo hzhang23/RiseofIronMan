5ebc3ffd0437ccbd2bb1c5f68e283a5d0f9b3f65
Ricky Zhang <hongxiaozh@gmail.com>
2020-08-28 11:08:58 -0700

    add file

--- src/com/starkIndustries/game/StoryTeller.java
+++ src/com/starkIndustries/game/StoryTeller.java
@@ -42,5 +42,5 @@ public class StoryTeller {
         System.out.println(player.get("voice").getAsString());
     }
 
-    //TODO: write a method to handle player's choice
+    //TODO: write a method to handle player's choice and able to redirect to next scene
 }
