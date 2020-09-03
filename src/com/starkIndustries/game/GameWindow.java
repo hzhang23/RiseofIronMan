e6ce84f3d6bd7c7c0eef83ee752b69d9042c7d03
Ricky Zhang <hongxiaozh@gmail.com>
2020-09-03 09:31:34 -0700

    add Closing window class

---
+++ src/com/starkIndustries/game/GameWindow.java
@@ -0,0 +1,8 @@
+package com.starkIndustries.game;
+
+import javax.swing.*;
+
+public class GameWindow extends JFrame {
+    // a window with 2 static display: bg picture and dialogue
+    // dialogue
+}
