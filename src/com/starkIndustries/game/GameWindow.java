60fbcb045b3487194e13210177005b5550118b82
Ricky Zhang <hongxiaozh@gmail.com>
2020-09-03 22:04:59 -0700

    refactor

--- src/com/starkIndustries/game/GameWindow.java
+++ src/com/starkIndustries/game/GameWindow.java
@@ -13,65 +13,85 @@ import java.util.ArrayList;
 import java.util.Set;
 
 public class GameWindow extends JFrame {
-    // a window with 2 static display: bg picture and dialogue box
-    // a button to click to continue the story
-    // when the questions pop up. I want to have click choose button or hyperlink to next scene
 
-    private ImageIcon bgImg;
-    private JLayeredPane layeredPane;
-    private JPanel bgPanel;
-    private JLabel bgLabel;
-    private JLabel sceneLabel;
-    private JButton nextBtn;
-    private JLabel chatLabel;
+    public GameWindow(String scene) {
+        //load JSON file
+        JsonObject myStory = StoryTeller.getStory(new File("resources/story.json"));
+        JsonObject thisScene = myStory.get(scene).getAsJsonObject();
+        String scenePicPath = thisScene.get("img").getAsString();
+        ArrayList<String> lines = getListStrFromJson(myStory, scene);
 
-    public GameWindow(){
-        layeredPane = new JLayeredPane();
-        bgImg = new ImageIcon("resources/StartGameBackground.jpg");
-        bgPanel = new JPanel();
-        bgPanel.setBounds(0,0,bgImg.getIconWidth(),bgImg.getIconHeight());
-        bgLabel = new JLabel(bgImg);
-        bgPanel.add(bgLabel);
-
-        //TODO: when load Json file, slice the path of scene picture to the value of sceneLabel
-        JsonObject scene = StoryTeller.getStory(new File("resources/story.json"));
-        String imgPath = scene.get("0").getAsJsonObject().get("img").getAsString();
-        ImageIcon sceneImg = new ImageIcon(imgPath);
-        sceneLabel = new JLabel(sceneImg);
+        //add bg pic
+        JLayeredPane layeredPane = new JLayeredPane();
+        JPanel bgPanel = getBgLabel();
+        layeredPane.add(bgPanel,JLayeredPane.DEFAULT_LAYER);
+        //add scene pic
+        ImageIcon sceneImg = new ImageIcon(scenePicPath);
+        JLabel sceneLabel = new JLabel(sceneImg);
         sceneLabel.setBounds(200, 50, 1000, 400);
         sceneLabel.setOpaque(true);
+        layeredPane.add(sceneLabel,JLayeredPane.MODAL_LAYER);
+        //add ChatLabel'
+        JTextArea chatArea = new JTextArea(2,20);
+        Font font = new Font("Anime Ace 2", Font.BOLD, 24);
+        chatArea.setBounds(200, 470, 800, 180);
+        chatArea.setFont(font);
+        chatArea.setWrapStyleWord(true);
+        chatArea.setLineWrap(true);
+        chatArea.setEditable(false);
+        chatArea.setOpaque(true);
+        chatArea.setBackground(Color.ORANGE);
+        chatArea.setText(lines.get(0));
+        layeredPane.add(chatArea,JLayeredPane.MODAL_LAYER);
 
-        chatLabel = new JLabel("This is a placeholder for Json script");
-        chatLabel.setBounds(200, 470, 800, 180);
-        chatLabel.setOpaque(true);
-        chatLabel.setBackground(Color.ORANGE);
-
-        nextBtn = new JButton("Continue");
+        //add nextBtn
+        JButton nextBtn = new JButton("Continue");
         nextBtn.setBounds(1050, 470,150,180);
         nextBtn.setBackground(Color.GREEN);
+        nextBtn.setFont(new Font("Arial", Font.PLAIN, 20));
+        layeredPane.add(nextBtn,JLayeredPane.MODAL_LAYER);
         nextBtn.addActionListener(new ActionListener() {
-            int i = 0;
+            int i = 1;
+            @Override
             public void actionPerformed(ActionEvent e) {
-                //runGameScript(new File("resources/story.json"));
-                JsonObject myStoryJson = StoryTeller.getStory(new File("resources/story.json"));
-                ArrayList<String> lines = getListStrFromJson(myStoryJson, "0");
                 if (i < lines.size()) {
-                    chatLabel.setText(lines.get(i));
+                    chatArea.setText(lines.get(i));
                     i++;
                 } else {
-                    playerChoice();
+                    String playerVoice = thisScene.get("Player").getAsJsonObject().get("voice").getAsString();
+                    chatArea.setText(playerVoice);
+                    JButton ABtn = new JButton("A");
+                    ABtn.addActionListener(new ActionListener() {
+                        String nextScene =
+                                thisScene.get("Player").getAsJsonObject()
+                                        .get("reply").getAsJsonObject().get("A").getAsString();
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            new GameWindow(nextScene);
+                            dispose();
+                        }
+                    });
+                    ABtn.setBounds(300, 600, 100, 50);
+                    layeredPane.add(ABtn,JLayeredPane.POPUP_LAYER);
+                    JButton BBtn = new JButton("B");
+                    BBtn.addActionListener(new ActionListener() {
+                        String nextScene =
+                                thisScene.get("Player").getAsJsonObject()
+                                        .get("reply").getAsJsonObject().get("B").getAsString();
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            new GameWindow(nextScene);
+                            dispose();
+                        }
+                    });
+                    BBtn.setBounds(450, 600, 100, 50);
+                    layeredPane.add(BBtn,JLayeredPane.POPUP_LAYER);
                 }
             }
         });
 
-        layeredPane.add(bgPanel,JLayeredPane.DEFAULT_LAYER);
-        layeredPane.add(sceneLabel,JLayeredPane.MODAL_LAYER);
-        layeredPane.add(chatLabel,JLayeredPane.MODAL_LAYER);
-        layeredPane.add(nextBtn,JLayeredPane.MODAL_LAYER);
-
-
         this.setLayeredPane(layeredPane);
-        this.setSize(bgImg.getIconWidth(), bgImg.getIconHeight());
+        this.setSize(bgPanel.getWidth(), bgPanel.getHeight());
         this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         this.setVisible(true);
         this.setResizable(false);
@@ -82,24 +102,7 @@ public class GameWindow extends JFrame {
                 new CloseWindow("Do you want to save the game before leave?");
             }
         });
-
     }
-
-    public void playerChoice() {
-        JsonObject scene = StoryTeller.getStory(new File("resources/story.json"));
-        JsonObject thisScene = scene.get("0").getAsJsonObject();
-        JsonObject playerReact = thisScene.get("Player").getAsJsonObject();
-        String playerVoice = playerReact.get("voice").getAsString();
-        chatLabel.setText(playerVoice);
-        JButton ABtn = new JButton("A");
-        ABtn.setBounds(300, 600, 100, 30);
-        layeredPane.add(ABtn,JLayeredPane.POPUP_LAYER);
-        JButton BBtn = new JButton("B");
-        BBtn.setBounds(450, 600, 100, 30);
-        layeredPane.add(BBtn,JLayeredPane.POPUP_LAYER);
-    }
-
-
 
     public ArrayList<String> getListStrFromJson (JsonObject script, String scene){
         ArrayList<String> linesToShow = new ArrayList<>();
@@ -113,8 +116,16 @@ public class GameWindow extends JFrame {
         return linesToShow;
     }
 
-    public static void main(String[] args) {
-        new GameWindow();
+    private JPanel getBgLabel(){
+        JPanel bgPanel = new JPanel();
+        ImageIcon bgImg = new ImageIcon("resources/StartGameBackground.jpg");
+        bgPanel.setBounds(0,0,bgImg.getIconWidth(),bgImg.getIconHeight());
+        JLabel bgLabel = new JLabel(bgImg);
+        bgPanel.add(bgLabel);
+        return bgPanel;
     }
 
+    public static void main(String[] args) {
+        new GameWindow("0");
+    }
 }
